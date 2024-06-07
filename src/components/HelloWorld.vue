bbdd68b4dd8bd05ca1bbcca1f75aff3af384a813
gonakano <g.nakano555@gmail.com>
2024-06-08 00:23:20 +0900

    feature3-commit1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@ const count = ref(0);
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
-      Edit this is feature1
+      Edit this is feature1, feature3 1st commit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
